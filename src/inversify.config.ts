b864ad76a8b6d6976f0e97e133c8ce38d1a1e76b
RBlanchet <contact@blanchet-romain.fr>
2021-04-04 13:35:46 +0200

    [feature] Ajout d'un Logger pour le CLI

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,9 +1,11 @@
 import {Container} from "inversify";
 import ConfigProvider from "./provider/config.provider";
 import DatabaseProvider from "./provider/database.provider";
+import Logger from "./cli/logger";
 
 const container = new Container();
 
+container.bind<Logger>(Logger).to(Logger);
 container.bind<ConfigProvider>(ConfigProvider).to(ConfigProvider);
 container.bind<DatabaseProvider>(DatabaseProvider).to(DatabaseProvider);
 
